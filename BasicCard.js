75e08288037882656cd9c703aaf0100b37f0094c
Monbedon <monbedon@gmail.com>
2017-07-20 01:13:11 -0700

    have prompts working

--- BasicCard.js
+++ BasicCard.js
@@ -1,12 +1,4 @@
 
-// var firstPresident = new BasicCard(
-//     "Who was the first president of the United States?", "George Washington");
-
-// // "Who was the first president of the United States?"
-// console.log(firstPresident.front); 
-
-// // "George Washington"
-// console.log(firstPresident.back); 
  var inquirer = require('inquirer');
 
  function BasicCard(front, back){
@@ -14,21 +6,18 @@
  	this.back = back;
 
  	this.printInfo= function(){
- 		console.log("Front: "+this.front+ "\nBack: "+this.back);
+ 		console.log("Front: "+this.front+"\nBack: "+this.back);
  	}
  }
 
 	inquirer.prompt([{
-		name: "front"
+		name: "front",
 		message: "Who was the first president of the United States?"
 	},{
-		name:"back"
+		name:"back",
 		message:"George Washington"
 	}]).then(function(answers){
 		var firstPresident = new BasicCard(answers.front, answers.back);
 		firstPresident.printInfo();
 	})
 
-}
-
-module.exports = BasicCard;
